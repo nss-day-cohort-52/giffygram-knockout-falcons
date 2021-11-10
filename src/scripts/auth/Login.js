58fadb1faa5fda53290b026e2476142c318b233d
jkuns76 <jkoonman@gmail.com>
2021-11-10 14:34:07 -0600

    pulling a main quick

--- src/scripts/auth/Login.js
+++ src/scripts/auth/Login.js
@@ -1,6 +1,5 @@
 import { getUsers } from "../data/provider.js"
-
-
+import { MessageForm } from "../message/MessageForm.js"
 document.addEventListener("click", clickEvent => {
     if (clickEvent.target.id === "loginButton") {
         let foundUser = null
@@ -37,5 +36,6 @@ export const LoginForm = () => {
             </form>
             <button id="loginButton">Login</button>
         </div>
+        ${MessageForm()}
     `
 }
