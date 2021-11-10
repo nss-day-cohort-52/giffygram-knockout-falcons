9317b3855867105ade50aa26226e61c25dad79e6
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-11-10 13:54:23 -0600

    added placeholder text for text areas in form

--- src/scripts/feed/PostForm.js
+++ src/scripts/feed/PostForm.js
@@ -5,13 +5,13 @@ export const PostForm = () => {
     return `
     <section class="post-form">
         <div class="field">
-            <textarea id="title" name="title" rows="1" cols="50">Title</textarea>
+            <textarea id="title" name="title" placeholder="Title" rows="1" cols="50">Title</textarea>
         </div>
         <div class="field">
-            <textarea id="url" name="url" rows="1" cols="50">URL of gif</textarea>
+            <textarea id="url" name="url" placeholder="URL of gif" rows="1" cols="50"></textarea>
         </div>
         <div class="field">
-            <textarea id="caption" name="caption" rows="3" cols="50">Story behind your gif...</textarea>
+            <textarea id="caption" name="caption" placeholder="Story behind your gif..." rows="3" cols="50"></textarea>
         </div>
 
         <button type="button" id="savePost">Save</button>
