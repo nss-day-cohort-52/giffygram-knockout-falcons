b72ab33a85fedd8ad8dbf358b823c7ae43ab8fb7
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-11-10 11:45:47 -0600

    fetch calls to save user, post, message, like and delete post and like

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -8,21 +8,108 @@ const applicationState = {
         chosenUser: null,
         displayFavorites: false,
         displayMessages: false
-        },
-    users: []
+    },
+    users: [],
+    likes: [],
+    posts: [],
+    messages: [],
+    follows: []
 }
 
 export const fetchUsers = () => {
     return fetch(`${API}/users`)
-    .then(response => response.json())
-    .then(
-        (login) => {
-            applicationState.users = login
-        }
-    )
+        .then(response => response.json())
+        .then(
+            (login) => {
+                applicationState.users = login
+            }
+        )
 }
 
 export const getUsers = () => {
-    return applicationState.users.map(user => ({...user}))
+    return applicationState.users.map(user => ({ ...user }))
 }
 
+
+
+export const createUser = (userObj) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userObj)
+    }
+
+    return fetch(`${API}/users`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const savePost = (postObj) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(postObj)
+    }
+
+    return fetch(`${API}/posts`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const saveMessage = (messageObj) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(messageObj)
+    }
+
+    return fetch(`${API}/messages`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const saveLike = (likeObj) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(likeObj)
+    }
+
+    return fetch(`${API}/likes`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+        })
+}
+
+export const deletePost = (id) => {
+    return fetch(`${API}/posts/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
+export const deleteLike = (id) => {
+    return fetch(`${API}/likes/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                applicationElement.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
